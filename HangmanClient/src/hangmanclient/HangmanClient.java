badf92d460a778b8f51328a82cd69bbddd8d0c1a
Lollodan <Lollodan@10.87.224.68>
2017-05-02 17:56:35 +0200

    client funzionante ma non si chiude a fine gioco

--- HangmanClient/src/hangmanclient/HangmanClient.java
+++ HangmanClient/src/hangmanclient/HangmanClient.java
@@ -5,20 +5,48 @@
  */
 package hangmanclient;
 
+    import java.io.*;
+    import java.net.*;
+    import java.util.*;
+import jdk.management.resource.ResourceType;
+
 /**
  *
  * @author Lollodan
  */
 public class HangmanClient {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here 
-        
-        
-        
-    }
+
     
-}
+
+     
+    
+    public static void main(String argv[]) {
+    BufferedReader in = null;
+    PrintStream out = null;
+    Socket socket = null;
+    String outputline = null;
+    String message;
+    try
+    {
+    java.io.BufferedReader console = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+    // open a socket connection
+    socket = new Socket("10.87.130.152", 5000);
+    // Apre i canali I/O
+    in = new BufferedReader(
+    new InputStreamReader(socket.getInputStream()));
+    out = new PrintStream(socket.getOutputStream(), true);
+    
+    while ((message = console.readLine()) != null) {
+        out.println(message);
+    }
+    out.close();
+    in.close();
+    }
+    catch(Exception e) { System.out.println(e.getMessage());}
+    }
+    }
+
+    
+    
+
